c6a76b6291050da5ac4d8eae531cb7c1a371f83e
anin-denin <anindeninadia@gmail.com>
2024-12-11 23:10:05 +0700

    seri Iphone

--- app/Livewire/EditProduct.php
+++ app/Livewire/EditProduct.php
@@ -7,38 +7,29 @@ use Livewire\Component;
 use App\Models\Category;
 use Livewire\WithFileUploads;
 
-//anin
 class EditProduct extends Component
 {
     use WithFileUploads;
     public $product_name = '';
-    public $photo = '';
+    public $photo;
     public $product_description = '';
     public $product_price = '';
-
-    //category 
     public $category_id;
-
     public $currentUrl;
-
     public $all_categories;
-
-    public $Product_details;
-
+    public $product_details;
     public function mount($id){
         $this->product_details = Product::find($id);
-
         $this->product_name = $this->product_details->name;
         $this->product_description = $this->product_details->description;
         $this->product_price = $this->product_details->price;
         $this->category_id = $this->product_details->category_id;
         $this->photo = $this->product_details->image;
-                // dd($this->product_details);
+        // dd($this->product_details);
         $this->all_categories = Category::all();
     }
-
     public function update(){
-        //validation
+        //validation 
         $this->validate([
             'product_name' => 'required|string|max:255',
             'product_description' => 'required|string',
@@ -46,9 +37,8 @@ class EditProduct extends Component
             'category_id' => 'required|exists:categories,id',
             'photo' => 'nullable|image|max:1024', // Validate the photo
         ]);
-
-         //check if the image update/uploaded
-         if ($this->photo && !is_string($this->photo)) {
+        //check if the image update/uploaded
+        if ($this->photo && !is_string($this->photo)) {
             $photoPath = $this->photo->store('photos', 'public');
         } else {
             $photoPath = $this->photo; // Keep the old image path
@@ -67,11 +57,10 @@ class EditProduct extends Component
     public function render()
     {
         // $current_url = url()->current();
-      
-        // $explode_url = explode('/', $current_url);
+        
+        // $explode_url = explode('/',$current_url);
         // // dd($explode_url);
-        // // Menggabungkan elemen ke-4 dan ke-5 dari URL saat ini dengan spasi di antaranya, lalu menyimpannya ke properti $currentUrl
-        // $this ->currentUrl = $explode_url[3].' '.$explode_url[4];
+        // $this->currentUrl = $explode_url[3].' '.$explode_url[5];
 
         return view('livewire.edit-product')
         ->layout('admin-layout');
